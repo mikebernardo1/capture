2ad98a312bf2cd61294e7e857f3294725b015575
Michael Bernardo <michael_bernardo@me.com>
2021-03-17 11:41:39 -0400

    faq styled component

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+
+import styled from 'styled-components';
+import {AboutStyle, DescriptionStyle, ImageStyle, HideStyle} from '../../styles/styles';
 
 const FAQ = () => {
    return (
-      <div className='faq'>
+      <FAQStyle>
          <h2>Any questions <span>FAQ</span></h2>
          <div className="question">
             <h4>How do I start?</h4>
@@ -10,6 +13,7 @@ const FAQ = () => {
                <p>Lorem ipsum dolor sit amet.</p>
                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque, impedit.</p>
             </div>
+            <div className="faq-line"></div>
          </div>
          <div className="question">
             <h4>Daily Schedule</h4>
@@ -17,6 +21,7 @@ const FAQ = () => {
                <p>Lorem ipsum dolor sit amet.</p>
                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque, impedit.</p>
             </div>
+            <div className="faq-line"></div>
          </div>
          <div className="question">
             <h4>Different payment methods</h4>
@@ -24,6 +29,7 @@ const FAQ = () => {
                <p>Lorem ipsum dolor sit amet.</p>
                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque, impedit.</p>
             </div>
+            <div className="faq-line"></div>
          </div>
          <div className="question">
             <h4>What products do you offer?</h4>
@@ -31,9 +37,37 @@ const FAQ = () => {
                <p>Lorem ipsum dolor sit amet.</p>
                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque, impedit.</p>
             </div>
+            <div className="faq-line"></div>
          </div>
-      </div>
+      </FAQStyle>
    )
 }
+
+const FAQStyle = styled(AboutStyle)`
+   display:block;
+   span{
+      display:block;
+   }
+   h2{
+      padding-bottom:2rem;
+      font-weight:lighter;
+   }
+   .faq-line{
+      background:#cccccc;
+      height: 0.2rem;
+      margin: 2rem 0rem;
+      width:100%;
+   }
+   .question{
+      padding: 3rem 0rem;
+      cursor: pointer;
+   }
+   .answer{
+      padding: 2rem 0rem;
+      p{
+         padding: 1rem 0rem;
+      }
+   }
+`;
 
 export default FAQ
